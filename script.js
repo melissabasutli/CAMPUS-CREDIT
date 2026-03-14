c04bb9fd78f9965188b051e8aecde18a91c32ff2
melissabasutli <bida25-322@thuto.bac.ac.bw>
2026-03-15 00:09:40 +0200

    update navigation links to point to index.html and modify sign-up redirection

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector('.signupbtn').addEventListener('click', function(event) {
     event.preventDefault();
     alert('Sign Up successful! Welcome to Campus Credit!');
-    window.location.href = 'home.html';
+    window.location.href = 'index.html';
 });
