0058b8e0568cc022c8474a1417782d2a4737c091
melissabasutli <bida25-322@thuto.bac.ac.bw>
2026-03-14 23:54:18 +0200

    add sign up page and signup functionality with modal

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.signupbtn').addEventListener('click', function(event) {
+    event.preventDefault();
+    alert('Sign Up successful! Welcome to Campus Credit!');
+    window.location.href = 'index.html';
+});
